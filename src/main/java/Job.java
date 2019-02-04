97a7eb7fbff8033192c34b59097d4a9470f94a55
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-02-04 13:43:29 -0200

    Métodos setter finalizados. Validações concluídas

--- src/main/java/Job.java
+++ src/main/java/Job.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class Job {
+    private String cargo;
+    private Double salario;
+
+    Scanner input = new Scanner(System.in);
+
+    public Job(String cargo, Double salario) {
+        if (cargo == null || cargo == "" || cargo == " ") {
+            System.out.println("Por favor informe um cargo válido");
+            setCargo(input.nextLine());
+        }
+
+        this.cargo = cargo;
+        if (salario == 0.0 || salario < 0) {
+            System.out.println("Por favor informe um salário válido");
+            setSalario(input.nextDouble());
+        }
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        if (cargo == null || cargo == "" || cargo == " ") {
+            System.out.println("Por favor informe um cargo válido");
+            setCargo(input.nextLine());
+        }
+        this.cargo = cargo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        if (salario == 0.0 || salario < 0) {
+            System.out.println("Por favor informe um cargo válido");
+            setSalario(input.nextDouble());
+        }
+        this.salario = salario;
+    }
 }
